6c3a97c0bed1ee663c547981cb9e443dca1b6561
Ron <odririse@gmail.com>
2026-01-03 15:52:46 +0100

    Update twitch-clips-redirect.users.js

--- twitch-clips-redirect.users.js
+++ twitch-clips-redirect.users.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Twitch Clip Redirect
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/OD728
 // @version      1.0
 // @description  Redirect clips from twitch.tv to clips.twitch.tv
 // @author       RM
@@ -24,4 +24,5 @@
         window.location.replace("https://clips.twitch.tv/" + match[1]);
     }
 })();
+
 
