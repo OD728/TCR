eb4b42f730f1596f13a499771870e0d9efe6bd5c
Ron <odririse@gmail.com>
2026-01-03 15:49:54 +0100

    Update twitch-clips-redirect.users.js

--- twitch-clips-redirect.users.js
+++ twitch-clips-redirect.users.js
@@ -4,13 +4,13 @@
 // @version      1.0
 // @description  Redirect clips from twitch.tv to clips.twitch.tv
 // @author       RM
-// @license      MIT
 // @match        *://*.twitch.tv/*/clip/*
 // @grant        none
-// @run-at       document-start
-// @homepage     https://github.com/OD728/TCR
-// @downloadURL  https://github.com/OD728/TCR/raw/main/twitch-clips-redirect.user.js
-// @updateURL    https://github.com/OD728/TCR/raw/main/twitch-clips-redirect.user.js
+// @license      MIT
+// @downloadURL  https://github.com/OD728/TCR/raw/refs/heads/main/twitch-clips-redirect.users.js
+// @updateURL    https://github.com/OD728/TCR/raw/refs/heads/main/twitch-clips-redirect.users.js
+// @supportURL   https://github.com/OD728/TCR/issues
+// @homepageURL  https://github.com/OD728/TCR
 // ==/UserScript==
 
 (function() {
