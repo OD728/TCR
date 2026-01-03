2d5b6f645418b7c31bde6f78dac58db8e74239a4
Ron <odririse@gmail.com>
2026-01-03 14:52:40 +0100

    Update twitch-clips-redirect.users.js

--- twitch-clips-redirect.users.js
+++ twitch-clips-redirect.users.js
@@ -4,6 +4,8 @@
 // @version      1.0
 // @description  Redirect clips from twitch.tv to clips.twitch.tv
 // @author       RM
+// @downloadURL  https://github.com/OD728/TCR/raw/refs/heads/main/twitch-clips-redirect.user.js
+// @updateURL    https://github.com/OD728/TCR/raw/refs/heads/main/twitch-clips-redirect.user.js
 // @homepageURL  https://github.com/OD728/TCR
 // @match        *://*.twitch.tv/*/clip/*
 // @grant        none
@@ -18,4 +20,5 @@
     if (match && match[1]) {
         window.location.replace("https://clips.twitch.tv/" + match[1]);
     }
+
 })();
